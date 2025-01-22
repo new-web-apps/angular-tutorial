318d9aded8f828138efc457b2b6f71679b108d75
new-web-apps <web-applications@yandex.ru>
2025-01-22 23:46:22 +0300

    add new component to app-component

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent],
   template: `
-     <h1>Hello world!</h1>
+    <main>
+      <header class="brand-name">
+        <img class="brand-logo" src="./assets/logo.jpg" alt="logo" aria-hidden="true" />
+      </header>
+      <section class="content">
+        <app-home></app-home>
+      </section>
+    </main>
   `,
   styleUrl: './app.component.scss'
 })
