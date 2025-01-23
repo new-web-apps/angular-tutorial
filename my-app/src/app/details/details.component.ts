0cbca61b1f063b00343416fa30709e108718b624
new-web-apps <web-applications@yandex.ru>
2025-01-23 10:02:11 +0300

    add form

--- my-app/src/app/details/details.component.ts
+++ my-app/src/app/details/details.component.ts
@@ -3,10 +3,11 @@ import { ActivatedRoute } from '@angular/router';
 import { HousingService } from '../housing.service';
 import { HousingLocation } from '../housinglocation';
 import {CommonModule} from '@angular/common';
+import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-details',
-  imports: [CommonModule], // работает и юез импорта CommonModule
+  imports: [CommonModule, ReactiveFormsModule], // работает и без импорта CommonModule
   templateUrl: './details.component.html',
   styleUrl: './details.component.scss'
 })
@@ -14,8 +15,20 @@ export class DetailsComponent {
   route: ActivatedRoute = inject(ActivatedRoute);
   HousingService = inject(HousingService)
   housingLocation: HousingLocation | undefined;
+  form = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl(''),
+  })
   constructor() {
     const housingLocationId = Number(this.route.snapshot.params['id']);
     this.housingLocation = this.HousingService.getHousingLocationById(housingLocationId);
+  }
+  submitApplication() {
+    this.HousingService.submitApplication(
+      this.form.value.firstName || '',
+      this.form.value.lastName || '',
+      this.form.value.email || '',
+    )
   }
 }
